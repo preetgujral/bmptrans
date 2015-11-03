8bfc46fee8773923b586896004fbff854afac204
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-11-03 14:59:29 -0800

    adding all files

---
+++ index.js
@@ -0,0 +1,24 @@
+//link from article - https://en.wikipedia.org/wiki/BMP_file_format
+
+//info on how to read image
+//http://www.javaworld.com/article/2077542/learn-java/java-tip-43--how-to-read-8--and-24-bit-microsoft-windows-bitmaps-in-java-applications.html
+
+//READ THIS
+//http://www.cs.sfu.ca/~ggbaker/165-draft/content/graphics-bitmap.html
+
+//this is the main transformer
+
+//require fs and assign to var
+var fs = require('fs');
+
+//TODO use fs to grab a file
+  //Need a file to grab. How do I choose a file?
+
+  //
+
+//create buffer and assign to var
+var buff = new Buffer();
+
+
+
+
