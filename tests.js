84db952467af5a22694d106fbbc5026566a058e8
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-11-11 14:57:24 -0800

    finished testing, gulp, and refactored code

--- tests.js
+++ tests.js
@@ -1,29 +1,22 @@
+'use strict';
+
 var expect = require('chai').expect;
+var fs = require('fs');
 var index = require('./index');
-var fs = require('fs')
 
-describe('transform bitmap file', function(){
-  describe('file system requests', function(){
-    it ('should do something', function(){
-      expect().to.be.a("number");
-    })
-    it('fileB should exist', function(){
-      //expect()....something();
+var fileA = './fileA.bmp';
+var fileB = './fileB.bmp';
+describe('bitmap transformer', function() {
+  it('given an imported file, should create fileB', function(done){
+    fs.readFile(fileB, function(err, data) {
+      expect(data).to.exist;
+      done();
     });
-    it('fileB size should equal fileA', function(){
-      //expect()....something();
+  });
+  it('should have correct length', function(done){
+    fs.readFile(fileB, function(err, data) {
+      expect(data.length).to.eql(4362);
+      done();
     });
-  })
-
-  describe('transformation', function(){
-    it ('sample exportColors number should equal importColor number', function(){
-      expect(index.importColor[1]).to.eql(255-index.exportColor[1]);
-    })
-    it('fileB should exist', function(){
-      //expect()....something();
-    });
-    it('fileB size should equal fileA', function(){
-      //expect()....something();
-    });
-  })
-})
+  });
+});
